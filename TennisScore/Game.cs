7387bdaf66345ec53d25dbb3519b4c2f749e8ae2
coolwuu <coolwuu0207@gmail.com>
2018-03-11 21:18:06 +0700

    Practice Completed

--- TennisScore/Game.cs
+++ TennisScore/Game.cs
@@ -1,11 +1,72 @@
-﻿namespace TennisScore
+﻿using System;
+using System.Collections.Generic;
+
+namespace TennisScore
 {
     public class Game
     {
-        public int SecondPlayerScore { get; set; }
+        private readonly Dictionary<int, string> _scoreLookUp = new Dictionary<int, string>
+        {
+            {0, "Love"},
+            {1, "Fifteen"},
+            {2, "Thirty"},
+            {3, "Forty"},
+        };
+
+        private const string Duece = "Duece";
+
+        public string FirstPlayerName { get; set; }
         public int FirstPlayerScore { get; set; }
         public int Id { get; set; }
-        public string FirstPlayerName { get; set; }
         public string SecondPlayerName { get; set; }
+        public int SecondPlayerScore { get; set; }
+
+        public bool IsSameScore()
+        {
+            return FirstPlayerScore == SecondPlayerScore;
+        }
+
+        public string ScoreResult()
+        {
+            return IsSameScore()
+                ? (IsDuece() ? Duece : SameScoreLookup())
+                : (IsReadyForWin() ? AdvanceState() : DiffScoreLookUp());
+        }
+
+        private string AdvanceState()
+        {
+            return IsAdvance() ? AdvPlayer() + " Adv" : AdvPlayer() + " Win";
+        }
+
+        private string AdvPlayer()
+        {
+            return FirstPlayerScore > SecondPlayerScore
+                ? FirstPlayerName
+                : SecondPlayerName;
+        }
+
+        private string DiffScoreLookUp()
+        {
+            return _scoreLookUp[FirstPlayerScore] + " " + _scoreLookUp[SecondPlayerScore];
+        }
+
+        private bool IsAdvance()
+        {
+            return Math.Abs(FirstPlayerScore - SecondPlayerScore) == 1;
+        }
+
+        private bool IsDuece()
+        {
+            return FirstPlayerScore >= 3;
+        }
+
+        private bool IsReadyForWin()
+        {
+            return FirstPlayerScore > 3 || SecondPlayerScore > 3;
+        }
+        private string SameScoreLookup()
+        {
+            return _scoreLookUp[FirstPlayerScore] + " All";
+        }
     }
 }
--- TennisScore/TennisGame.cs
+++ TennisScore/TennisGame.cs
@@ -6,15 +6,6 @@ namespace TennisScore
     public class TennisGame
     {
         private readonly IRepository<Game> _repo;
-
-        private readonly Dictionary<int, string> _scoreLookUp = new Dictionary<int, string>
-        {
-            {0, "Love"},
-            {1, "Fifteen"},
-            {2, "Thirty"},
-            {3, "Forty"},
-        };
-
         public TennisGame(IRepository<Game> repo)
         {
             _repo = repo;
@@ -23,42 +14,7 @@ namespace TennisScore
         public string ScoreResult(int gameId)
         {
             var game = _repo.GetGame(gameId);
-            if (IsSameScore(game))
-            {
-                if (IsDuece(game))
-                    return "Duece";
-                return SameScoreLookup(game);
-            }
-
-            if (game.FirstPlayerScore > 3 || game.SecondPlayerScore > 3)
-            {
-                if (Math.Abs(game.FirstPlayerScore - game.SecondPlayerScore) == 1)
-                {
-                    return (game.FirstPlayerScore > game.SecondPlayerScore
-                               ? game.FirstPlayerName
-                               : game.SecondPlayerName) + " Adv";
-                }
-
-                return (game.FirstPlayerScore > game.SecondPlayerScore
-                           ? game.FirstPlayerName
-                           : game.SecondPlayerName) + " Win";
-            }
-            return _scoreLookUp[game.FirstPlayerScore] + " " + _scoreLookUp[game.SecondPlayerScore];
-        }
-
-        private static bool IsDuece(Game game)
-        {
-            return game.FirstPlayerScore >= 3;
-        }
-
-        private static bool IsSameScore(Game game)
-        {
-            return game.FirstPlayerScore == game.SecondPlayerScore;
-        }
-
-        private string SameScoreLookup(Game game)
-        {
-            return _scoreLookUp[game.FirstPlayerScore] + " All";
+            return game.ScoreResult();
         }
     }
 }
