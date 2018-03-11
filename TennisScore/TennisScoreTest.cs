cbc33d289d424c5d77fdf87e177b15c8e549648b
coolwuu <coolwuu0207@gmail.com>
2018-03-11 20:12:02 +0700

    1.Introduce field for variable game id and repository 2. Extract method as GivenScore

--- TennisScore/TennisScoreTest.cs
+++ TennisScore/TennisScoreTest.cs
@@ -6,18 +6,23 @@ namespace TennisScore
     [TestClass]
     public class TennisScoreTest
     {
+        private readonly int _anyGameId = 1;
+        private readonly IRepository<Game> _repository = Substitute.For<IRepository<Game>>();
+
         [TestMethod]
         public void Love_All()
         {
-            var gameId = 1;
+            GivenScore(0, 0);
 
-            IRepository<Game> repo = Substitute.For<IRepository<Game>>();
-            repo.GetGame(gameId).Returns(new Game { Id = gameId, FirstPlayerScore = 0, SecondPlayerScore = 0 });
+            TennisGame tennisGame = new TennisGame(_repository);
 
-            TennisGame tennisGame = new TennisGame(repo);
-
-            var scoreResult = tennisGame.ScoreResult(gameId);
+            var scoreResult = tennisGame.ScoreResult(_anyGameId);
             Assert.AreEqual("Love All", scoreResult);
+        }
+
+        private void GivenScore(int secondPlayerScore, int firstPlayerScore)
+        {
+            _repository.GetGame(_anyGameId).Returns(new Game {Id = _anyGameId, FirstPlayerScore = firstPlayerScore, SecondPlayerScore = secondPlayerScore});
         }
     }
 }
