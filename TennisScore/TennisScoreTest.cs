02ffa4e35a39c87cbd9f44cffecd2abdf12a7f2d
coolwuu <coolwuu0207@gmail.com>
2018-03-11 20:15:40 +0700

    introduce field for tennisGame and extract it's initial to a method called TestInit. Extract method of Assertion of test to ScoreShouldBe

--- TennisScore/TennisScoreTest.cs
+++ TennisScore/TennisScoreTest.cs
@@ -8,16 +8,25 @@ namespace TennisScore
     {
         private readonly int _anyGameId = 1;
         private readonly IRepository<Game> _repository = Substitute.For<IRepository<Game>>();
+        private TennisGame _tennisGame;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _tennisGame = new TennisGame(_repository);
+        }
 
         [TestMethod]
         public void Love_All()
         {
             GivenScore(0, 0);
+            ScoreShouldBe("Love All");
+        }
 
-            TennisGame tennisGame = new TennisGame(_repository);
-
-            var scoreResult = tennisGame.ScoreResult(_anyGameId);
-            Assert.AreEqual("Love All", scoreResult);
+        private void ScoreShouldBe(string expected)
+        {
+            var scoreResult = _tennisGame.ScoreResult(_anyGameId);
+            Assert.AreEqual(expected, scoreResult);
         }
 
         private void GivenScore(int secondPlayerScore, int firstPlayerScore)
